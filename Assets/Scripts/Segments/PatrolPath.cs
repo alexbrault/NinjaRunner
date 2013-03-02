d5dbf6885b52f5c8ecd27c1a25c8bc2954ab927e
Alexandre Brault <alex@3doubloons.net>
2013-03-02 10:56:01 +0000

    Better Path Gizmo

--- Assets/Scripts/Segments/PatrolPath.cs
+++ Assets/Scripts/Segments/PatrolPath.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 using System.Collections;
 
 public class PatrolPath : MonoBehaviour {
+	private Transform[] patrolPoints;
+	private void Awake() {
+		patrolPoints = new Transform[2];
+		patrolPoints[0] = transform.GetChild(0);
+		patrolPoints[1] = transform.GetChild(1);
+	}
 	private void OnDrawGizmos() {
 		Gizmos.color = Color.green;	
 		
-		Transform[] patrolPoints = new Transform[2];
-		patrolPoints[0] = transform.GetChild(0);
-		patrolPoints[1] = transform.GetChild(1);
-		
 		Gizmos.DrawLine(patrolPoints[0].position, patrolPoints[1].position);
+		Gizmos.DrawCube(patrolPoints[0].position, Vector3.one);
+		Gizmos.DrawCube(patrolPoints[1].position, Vector3.one);
 	}
 }
