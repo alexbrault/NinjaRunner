431ceea09bf48bcada430482e9d1385bb9f35358
Alexandre Brault <alex@3doubloons.net>
2013-03-02 10:51:31 +0000

    Montrer le chemin de patrouille comme Gizmo

--- Assets/Scripts/Segments/PatrolPath.cs
+++ Assets/Scripts/Segments/PatrolPath.cs
@@ -2,4 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class PatrolPath : MonoBehaviour {
+	private void OnDrawGizmos() {
+		Gizmos.color = Color.green;	
+		
+		Transform[] patrolPoints = new Transform[2];
+		patrolPoints[0] = transform.GetChild(0);
+		patrolPoints[1] = transform.GetChild(1);
+		
+		Gizmos.DrawLine(patrolPoints[0].position, patrolPoints[1].position);
+	}
 }
