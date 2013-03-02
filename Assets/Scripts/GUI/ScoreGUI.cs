4f69397144e8a38667f15289c2842ed705cf0f5c
Frederick Imbeault <fimbeault@gmail.com>
2013-03-02 16:22:47 +0000

    Cool font score

--- Assets/Scripts/GUI/ScoreGUI.cs
+++ Assets/Scripts/GUI/ScoreGUI.cs
@@ -4,24 +4,26 @@ using System.Collections;
 public class ScoreGUI : MonoBehaviour {
 	
 	public int noPlayer = 1;
+	public Font textFont;
 	
-	// Use this for initialization
-	void Start () 
+	private Camera cam;
+	private GUIStyle textStyle;
+
+	void Start ()
 	{
 		noPlayer = this.gameObject.GetComponent<NinjaController>().Player;
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
+		
+		cam = gameObject.transform.FindChild("Camera").gameObject.camera;
+		
+		textStyle = new GUIStyle();
+		textStyle.font = textFont;
+		textStyle.fontSize = 28;
+		textStyle.normal.textColor = Color.red;
 	}
 	
  	void OnGUI ()
-	{
-		GUI.Label( new Rect(0, gameObject.transform.FindChild("Camera").gameObject.camera.pixelRect.y, 200, 50),
-				"Player " + (noPlayer+1) + " : " + ScoreManager.Instance.GetScore(noPlayer+1));
-				
-		//GUI.Label(new Rect(0,Screen.height/noPlayer + 20, 200, 50), "Time : " + playerTime);
+	{		
+		GUI.Label( new Rect(20, Mathf.Abs(cam.pixelRect.y - cam.pixelHeight) + 10, 200, 50),
+				"Player " + (noPlayer + 1) + " : " + ScoreManager.Instance.GetScore(noPlayer), textStyle);
 	}
 }
