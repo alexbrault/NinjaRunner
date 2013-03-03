7de923081a32f6fdd8f9581ce89106dd446cf25c
Frederic Boucher <fred.boucher555@gmail.com>
2013-03-03 23:52:13 +0000

    <No comment>

--- Assets/Scripts/GUI/ScoreGUI.cs
+++ Assets/Scripts/GUI/ScoreGUI.cs
@@ -29,7 +29,11 @@ public class ScoreGUI : MonoBehaviour {
 	
  	void OnGUI ()
 	{		
-		GUI.Label( new Rect(20, 10 + noPlayer*(Screen.height/2), 200, 50),
+		if(noPlayer == 0)
+			GUI.Label( new Rect(20 , 10, 200, 50),
 				"Player " + (noPlayer + 1) + " : " + ScoreManager.Instance.GetScore(noPlayer), textStyle);
+		else
+			GUI.Label( new Rect(Screen.width - 200, Screen.height-50, 200, 50),
+				"Player " + (noPlayer + 1) + " : " + ScoreManager.Instance.GetScore(noPlayer), textStyle);			
 	}
 }
